afdd8ce4dd67539e8dfab035723aa824331de152
443556 <modeck1@yahoo.ie>
2015-07-05 21:07:23 +0100

    Fix for 'ImportError: cannot import name timezone' in blog/models.py

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from django.utils import timezone
+from django.utils import datetime
 
 class Post(models.Model):
     
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
+    created_date = models.DateTimeField(default=datetime.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
     def pusblish(self):
